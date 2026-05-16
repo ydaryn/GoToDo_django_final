24cb95211aed455791469f16d5adee3733c2a6a3
Yerdaulet <y_toktagul@kbtu.kz>
2026-05-16 11:27:00 +0500

    feat(auth): implement custom user, JWT login/refresh, and custom permissions

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Подключаем все эндпоинты из accounts (логин, регистр, профиль, рефреш)
+    path('api/v1/', include('apps.accounts.urls')),
 ]
